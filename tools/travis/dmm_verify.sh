0ad00686b22dd493aaddcec89f6ad517f934ebc3
kevinz000 <2003111+kevinz000@users.noreply.github.com>
2019-01-10 10:45:09 -0800

    wrong dir

--- tools/travis/dmm_verify.sh
+++ tools/travis/dmm_verify.sh
@@ -11,7 +11,7 @@ if grep 'step_[x]' maps/**/*.dmm; then
 	echo "Step_x and step_y are not allowed in .dmm files, as pixel movement is not implemented."
 	exit 1
 fi;
-if grep -E '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
+if grep -E '^\".+\" = \(.+\)' maps/**/*.dmm;	then
 	echo "Non-TGM formatted map detected. Please convert it using Map Merger!"
 	exit 1
 fi;
