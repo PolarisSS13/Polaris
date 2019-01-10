02d5041425175c6e848cc9aee3ecc4b4b7441bf1
kevinz000 <2003111+kevinz000@users.noreply.github.com>
2019-01-10 10:07:30 -0800

    Enforced TGM checks

---
+++ tools/travis/dmm_verify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+shopt -s globstar
+
+if grep -Pn '( |\t|;|{)tag( ?)=' maps/**/*.dmm;	then
+	echo "Tags are not allowed in .dmm files."
+	exit 1
+fi;
+if grep 'step_[x]' maps/**/*.dmm; then
+	echo "Step_x and step_y are not allowed in .dmm files, as pixel movement is not implemented."
+	exit 1
+fi;
+if grep -E '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
+	echo "Non-TGM formatted map detected. Please convert it using Map Merger!"
+	exit 1
+fi;
