4a8866c7fb7cf26a8d7674083aa31fe16b1df74a
Spookerton <spkrtn@pm.me>
2025-07-09 16:38:47 +0100

    update various dependencies
    sdmm 1.7 -> 1.9
    Also updated sdmm config for future strictness changes
    
    rust_g 0.46.x -> 3.9.0
    Ships the 3.9.0 windows dll for desktop dev. Removed the .so because
    new versions are a large binary blob; instead these are fetched from
    tgstation release build artifacts for tests, and can be collected or
    built locally for linux hosting.
    
    Workflow OS targets updated from ubuntu-20 -> ubuntu-24
    
    stefanzweifel/git-auto-commit-action v5 -> v6 to satisfy dependabot
    
    Mislabeled actions/checkout 3 -> 4
    
    Added cancel-in-progress concurrency rule to CI & label workflows. This
    causes new runs of the workflows to cancel older runs still in progress
    for a given ref (branch, pr, etc)
    
    Where not using an official action, uses commit hash targets instead of
    tags

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -1,6 +1,6 @@
 # This file has all the information on what versions of libraries are thrown into the code
 # For dreamchecker
-export SPACEMAN_DMM_VERSION=suite-1.7.3
+export SPACEMAN_DMM_VERSION=suite-1.9
 # For NanoUI + TGUI
 export NODE_VERSION=12
 # Byond Major
@@ -8,4 +8,6 @@ export BYOND_MAJOR=514
 # Byond Minor
 export BYOND_MINOR=1589
 # Macro Count
-export MACRO_COUNT=4
+export MACRO_COUNT=0
+# rustg
+export RUST_G_VERSION=3.9.0
